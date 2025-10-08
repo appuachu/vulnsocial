f9963c0a02658811660ab968ecc4d9a4fcd71f9a
Achu <87751449+appuachu@users.noreply.github.com>
2025-10-09 02:10:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -913,4 +913,4 @@ def admin_toggle_user_status(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5010)
